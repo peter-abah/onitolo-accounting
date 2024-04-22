1df2349ae54a0525f8c0619e4ebcf7c99b832d6f
Peter Abah <peterabah.ob@gmail.com>
2024-04-22 18:40:44 +0100

    Remove benefits section and restyle contact section

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 
 const mobileNavBtn = document.getElementById("mobile-nav-btn");
 const mobileNav = document.getElementById("mobile-nav");
+const mobileNavLinks = document.querySelectorAll(".mobile-nav__link");
 
 // Toggle mobile nav
 mobileNavBtn?.addEventListener("click", (e) => {
   const isOpen = mobileNav.dataset.isOpen === "true";
   mobileNav.dataset.isOpen = !isOpen;
 });
+
+// Close mobile nav when link in nav is clicked
+mobileNavLinks.forEach((link) =>
+  link.addEventListener("click", () => {
+    mobileNav.dataset.isOpen = false;
+  })
+);
